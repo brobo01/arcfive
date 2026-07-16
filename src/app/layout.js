3e412574bcded342ca13ad598edf554e4539fa2a
Ben Roberts <benroberts67@hotmail.co.uk>
2026-07-16 15:38:29 +0100

    Added gtag manager

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import Script from "next/script"
 import "./globals.css"
 
 export const metadata = {
@@ -6,10 +7,37 @@ export const metadata = {
     "A five-day investigation into the death of Ayrton Senna at Imola, 1994.",
 }
 
+const GA_MEASUREMENT_ID = "GTM-K5Z429SJ"
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <head>
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+          strategy="afterInteractive"
+        />
+        <Script id="ga4-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_MEASUREMENT_ID}');
+          `}
+        </Script>
+      </head>
+
+      <body>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-ABCDEFGH"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        {children}
+      </body>
     </html>
   )
 }
